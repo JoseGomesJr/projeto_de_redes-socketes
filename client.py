21dccc8eb278e4042a8229e41bcbd173c58042b2
Leonardo Zaia <57048196+leozaia-h@users.noreply.github.com>
2022-06-16 00:40:54 -0300

    messages received
    mensagens recebidas no servidor

--- client.py
+++ client.py
@@ -1,9 +1,18 @@
+#!/bin/python3
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.0.3', 8080))
-msg = "xabusco no bitico"
-bMsg = msg.encode()
-client.send(bMsg)
-from_server = client.recv(4096)
+client.connect(('192.168.0.6', 8080))
+
+welc = str(input("Digite seu nome: "))
+bwelc = welc.encode()
+client.send(bwelc)
+
+while(True):
+        msg = str(input("msg: "))
+        if(msg == "xaxa"):
+                break
+        bMsg = msg.encode()
+        client.send(bMsg)
+
 client.close()
-print(from_server)
+#print(from_server)
--- server.py
+++ server.py
@@ -1,15 +1,31 @@
+#!/bin/python2.7
 import socket
+from threading import Thread
+
+
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('192.168.0.3', 8080))
+serv.bind(('192.168.0.6', 8080))
 serv.listen(5)
-while True:
-    conn, addr = serv.accept()
-    from_client = ''
+
+def task():
     while True:
-        data = conn.recv(4096)
-        if not data: break
-        from_client += data
-        print(from_client)
-        conn.send("I am SERVER")
-    conn.close()
-    print ("client disconnected")
+        conn, addr = serv.accept()
+        from_client = ''
+        welc_user = conn.recv(4096)
+        print("Welcome " + welc_user)
+
+        while True:
+            data = conn.recv(4096)
+            if not data: break
+            from_client = data
+            print(welc_user + ": " +from_client)
+
+        conn.close()
+        print (welc_user + " disconnected")
+
+
+thread1 = Thread(target=task)
+thread2 = Thread(target=task)
+
+thread1.start()
+thread2.start()
