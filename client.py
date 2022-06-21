f01d9886a3d05424652a713c1c731e25778f05a2
JoseGomes <59032543+JoseGomesJr@users.noreply.github.com>
2022-06-21 20:30:07 -0300

    teste

--- client.py
+++ client.py
@@ -1,9 +1,52 @@
+#!/bin/python3
+from ast import Str
+from re import T
+import re
 import socket
+
+FORMAT = "utf-8"
+
+def send_file(filename):
+    try:
+        file = open(filename, 'rb')
+    except PermissionError:
+        print("Not Can't open file")
+        return
+    else:
+        with file:
+                bytes = file.read()
+                str = "RECV"
+                bMsg_f = str.encode("utf-8")
+                client.send(bMsg_f)
+                client.sendall(bytes)
+
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.0.3', 8080))
-msg = "xabusco no bitico"
-bMsg = msg.encode()
-client.send(bMsg)
-from_server = client.recv(4096)
+client.connect(('127.0.0.1', 8080))
+
+welc = str(input("Digite seu nome: "))
+bwelc = welc.encode()
+client.send(bwelc)
+
+def recv():
+        str1 = "ENV"
+        to_receive = 50
+        bMsg_f = str1.encode("utf-8")
+        client.send(bMsg_f)
+        test= open("olha.txt", 'w')
+        bytes_read = client.recv(min(to_receive, 4096)).decode(FORMAT)
+        test.write( str(bytes_read.encode(FORMAT)) )
+
+
+while(True):
+        msg = str(input("msg: "))
+        if(msg == "RECV"):
+             recv()
+        elif (msg == "ENV"):
+                msg = str(input("NOME: "))   
+                send_file(msg)
+
 client.close()
-print(from_server)
+
+
+#print(from_server)
--- server.py
+++ server.py
@@ -1,15 +1,44 @@
+#!/bin/python2.7
+from cmath import e
 import socket
+from threading import Thread
+
+
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('192.168.0.3', 8080))
+serv.bind(('127.0.0.1', 8080))
 serv.listen(5)
-while True:
-    conn, addr = serv.accept()
-    from_client = ''
+
+
+def send_file(file, conn):
+    print("aquiii")
+    bytes_read = conn.recv(4096)              
+    file.write(bytes_read)
+
+def task():
     while True:
-        data = conn.recv(4096)
+        conn, addr = serv.accept()
+        from_client = ''
+        welc_user = conn.recv(4096)
+        print("Welcome " + str(welc_user))
+        
+        data = conn.recv(4096).decode('utf8')
         if not data: break
-        from_client += data
-        print(from_client)
-        conn.send("I am SERVER")
+        from_client = data
+        if from_client == 'RECV':
+            arq = open("buff", 'wb+')
+            send_file(arq, conn)
+            arq.close()
+        elif from_client == 'ENV':
+            arq = open("buff", 'rb+')
+            s = arq.read()
+            conn.sendall(s)
+            arq.close()
+
     conn.close()
-    print ("client disconnected")
+    print (str(welc_user) + " disconnected")
+
+
+thread1 = Thread(target=task)
+thread2 = Thread(target=task)
+thread1.start()
+thread2.start()
