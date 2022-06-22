a7f593400b6b30b7580cb7a79bbff4bee7722d53
JoseGomes <59032543+JoseGomesJr@users.noreply.github.com>
2022-06-22 11:28:46 -0300

    Correção no envio de arquivos

--- client.py
+++ client.py
@@ -1,10 +1,15 @@
 #!/bin/python3
-from ast import Str
+import os
 from re import T
 import re
 import socket
+import time
 
 FORMAT = "utf-8"
+MSG_SEP = "|"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 8080))
 
 def send_file(filename):
     try:
@@ -18,35 +23,48 @@ def send_file(filename):
                 str = "RECV"
                 bMsg_f = str.encode("utf-8")
                 client.send(bMsg_f)
+                file_size = os.path.getsize(filename)
+                print(file_size)
+                client.send(f"{file_size}{MSG_SEP}{filename}".encode(FORMAT))
+                time.sleep(0.5)
                 client.sendall(bytes)
-
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 8080))
-
-welc = str(input("Digite seu nome: "))
-bwelc = welc.encode()
-client.send(bwelc)
 
 def recv():
         str1 = "ENV"
         to_receive = 50
         bMsg_f = str1.encode("utf-8")
         client.send(bMsg_f)
-        test= open("olha.txt", 'w')
-        bytes_read = client.recv(min(to_receive, 4096)).decode(FORMAT)
-        test.write( str(bytes_read.encode(FORMAT)) )
+        filesize, filename = client.recv(4096).decode(FORMAT).split(MSG_SEP, 1)
+        to_receive = int(filesize)
+        with open(filename, 'wb+') as file:
+                print(filename)
+                while True:
+                        if to_receive > 0:
+                                bytes_read = client.recv(min(to_receive, 4096))               
+                                file.write(bytes_read)
+                                to_receive -= len(bytes_read)
+                        else:
+                                break
+        #bytes_read = client.recv(min(to_receive, 4096)).decode(FORMAT)
+        #test.write( str(bytes_read.encode(FORMAT)) )
 
 
-while(True):
-        msg = str(input("msg: "))
-        if(msg == "RECV"):
-             recv()
-        elif (msg == "ENV"):
-                msg = str(input("NOME: "))   
-                send_file(msg)
+def main():
 
-client.close()
+        welc = str(input("Digite seu nome: "))
+        bwelc = welc.encode()
+        client.send(bwelc)
+        while(True):
+                msg = str(input("msg: "))
+                if(msg == "RECV"):
+                        recv()
+                elif (msg == "ENV"):
+                        msg = str(input("NOME: "))   
+                        send_file(msg)
+                elif (msg == "END"):
+                        client.close()
 
+if __name__ == "__main__":
+    main()
 
 #print(from_server)
--- server.py
+++ server.py
@@ -1,19 +1,60 @@
 #!/bin/python2.7
-from cmath import e
+from re import T
 import socket
+import sys
 from threading import Thread
+from turtle import delay
 
+FORMAT = "utf-8"
+MSG_SEP = "|"
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('127.0.0.1', 8080))
 serv.listen(5)
 
+class File():
+    def __init__(self, filename, filesize):
+        self.filename = filename
+        self.filesize = filesize
+
+    def white_arq(self, to_receive, conn):
+
+        with open("buff", 'wb+') as file:
+            while True:
+                if to_receive > 0:
+                    print("to_receive")
+                    bytes_read = conn.recv(min(to_receive, 4096))               
+                    file.write(bytes_read)
+                    to_receive -= len(bytes_read)
+                else:
+                    break
+
+    def read_arq(self):
+        arq = open("buff", 'rb+')
+        s = arq.read()
+        arq.close()
+        return s
+
+arq = File(" ", 0 )
+
 
 def send_file(file, conn):
     print("aquiii")
-    bytes_read = conn.recv(4096)              
-    file.write(bytes_read)
+    filesize, file.filename =  (conn.recv(4096).decode(FORMAT).split(MSG_SEP, 1))
 
+    to_receive = int(filesize)
+    file.filesize = to_receive
+
+    file.white_arq(to_receive, conn)
+
+def recv_file(file, conn):
+    if file.filename == " " or file.filesize == 0:
+        return False
+    print(file.filename)
+    conn.send(f"{file.filesize}{MSG_SEP}{file.filename}".encode(FORMAT))
+    s = file.read_arq()
+    conn.sendall(s)
+    return True
 def task():
     while True:
         conn, addr = serv.accept()
@@ -25,20 +66,20 @@ def task():
         if not data: break
         from_client = data
         if from_client == 'RECV':
-            arq = open("buff", 'wb+')
             send_file(arq, conn)
-            arq.close()
         elif from_client == 'ENV':
-            arq = open("buff", 'rb+')
-            s = arq.read()
-            conn.sendall(s)
-            arq.close()
+            if not recv_file(arq, conn):
+                print("Nenhum arquivo encontrado")
+            else:
+                print("Arquivo enviado")
 
     conn.close()
     print (str(welc_user) + " disconnected")
 
-
-thread1 = Thread(target=task)
-thread2 = Thread(target=task)
-thread1.start()
-thread2.start()
+if __name__ == "__main__":
+    thread1 = Thread(target=task)
+    thread2 = Thread(target=task)
+    thread1.start()
+    thread2.start()
+    input("Pressione entre")
+    sys.exit()
